1961ce46b1e602699d26a3fb054b83f4fc345c3c
mu-mx <1017182993@qq.com>
2024-02-21 19:02:15 +0800

    aaa

--- scripts/template-ts/router/routes.ts
+++
@@ -1,9 +0,0 @@
-import { RouteRecordRaw } from 'vue-router'
-export const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/home',
-    name: 'index',
-    component: () => import('@projects/页面名称/views/index.vue'),
-    meta: { title: '首页' }
-  }
-]
