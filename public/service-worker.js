bbeba913bdf32fb24210b6e62c485119d99caab8
mu-mx <1017182993@qq.com>
2024-04-03 14:32:39 +0800

    pwa

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,23 @@
 self.addEventListener("install", (event) => {
-    console.log('event - >:', event)
+    console.log("event - >:", event);
     console.log("Service worker installing...");
     // Here, you'll later add code for actions like caching assets.
+
+    e.waitUntil(
+        caches.open("store").then(function (cache) {
+            console.log("Opened cache");
+            return cache.addAll([
+                "./",
+               './index.html',
+               './manifest.json',
+               './service-worker.js',
+               './vite.svg',
+               './images/homescreen144.png',
+               './images/homescreen168.png',
+               './images/homescreen48.png',
+               './images/homescreen72.png',
+               './images/homescreen96.png',
+            ]);
+        })
+    );
 });
