10eea2048187b027788eb9bc06b3b5a4271e2048
mu-mx <1017182993@qq.com>
2024-04-06 22:18:51 +0800

    sqsqsw

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,32 +1,25 @@
-self.addEventListener("install", (event) => {
-    console.log("event - >:", event);
-    console.log("Service worker installing...");
-    // Here, you'll later add code for actions like caching assets.
+const CACHE_NAME = "my-pwa-cache-v1";
+const urlsToCache = [
+    "/",
+    "/index.html",
+    "index.js",
+    "index.css",
+    "vite.svg",
+    "manifest.json",
+    "images/homescreen144.png",
+    "images/homescreen168.png",
+    "images/homescreen48.png",
+    "images/homescreen72.png",
+    "images/homescreen96.png",
+];
 
-    event.waitUntil(
-        caches
-            .open("store")
-            .then(function (cache) {
-                console.log("Opened cache");
-                return cache.addAll([
-                    "/",
-                    "/index.html",
-                    "index.js",
-                    "index.css",
-                    "vite.svg",
-                    "images/homescreen144.png",
-                    "images/homescreen168.png",
-                    "images/homescreen48.png",
-                    "images/homescreen72.png",
-                    "images/homescreen96.png",
-                ]);
-            })
-            .then(function () {
-                self.skipWaiting();
-            })
-            .catch(function (error) {
-                console.error("Cache failed:", error);
-            })
+self.addEventListener("install", (event) => {
+    event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => response || fetch(event.request))
     );
 });
 
