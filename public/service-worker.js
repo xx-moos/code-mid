a6cfd5be39f17388ec72b99bc206fae46d35d94b
mu-mx <1017182993@qq.com>
2024-04-06 21:41:39 +0800

    fbdfbdf

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,21 +3,29 @@ self.addEventListener("install", (event) => {
     console.log("Service worker installing...");
     // Here, you'll later add code for actions like caching assets.
 
-    e.waitUntil(
-        caches.open("store").then(function (cache) {
-            console.log("Opened cache");
-            return cache.addAll([
-                "./",
-               './index.html',
-               './manifest.json',
-               './service-worker.js',
-               './vite.svg',
-               './images/homescreen144.png',
-               './images/homescreen168.png',
-               './images/homescreen48.png',
-               './images/homescreen72.png',
-               './images/homescreen96.png',
-            ]);
-        })
+    event.waitUntil(
+        caches
+            .open("store")
+            .then(function (cache) {
+                console.log("Opened cache");
+                return cache.addAll([
+                    "/",
+                    "/assets/*.js",
+                    "/assets/*.css",
+                    "/index.html",
+                    "/manifest.json",
+                    "/service-worker.js",
+                    "/vite.svg",
+                    "/images/*.png",
+                    //    './images/homescreen144.png',
+                    //    './images/homescreen168.png',
+                    //    './images/homescreen48.png',
+                    //    './images/homescreen72.png',
+                    //    './images/homescreen96.png',
+                ]);
+            })
+            .catch(function (error) {
+                console.error("Cache failed:", error);
+            })
     );
 });
--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(<App />);
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", () => {
         navigator.serviceWorker
-            .register("./../service-worker.js")
+            .register("./service-worker.js")
             .then((registration) => {
                 console.log("Service Worker registered! Scope: ", registration.scope);
             })
