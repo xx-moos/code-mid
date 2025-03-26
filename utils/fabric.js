6735cb762771db95fecdc3a50889a813e1ee9b49
xx-moos <1017182993@qq.com>
2025-03-26 23:21:58 +0800

    置空

--- utils/fabric.js
+++
@@ -1,9 +0,0 @@
-let fabric = null;
-
-export async function getFabric() {
-  if (fabric) return fabric;
-
-  const fabricModule = await import("fabric");
-  fabric = fabricModule.fabric;
-  return fabric;
-}
