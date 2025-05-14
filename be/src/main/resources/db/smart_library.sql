0189581b90bf1973b811ea4f1346263afcdc619b
xx-moos <1017182993@qq.com>
2025-05-14 21:06:59 +0800

    aw

--- be/src/main/resources/db/smart_library.sql
+++ be/src/main/resources/db/smart_library.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80042
  File Encoding         : 65001
 
- Date: 14/05/2025 11:12:04
+ Date: 14/05/2025 21:06:49
 */
 
 SET NAMES utf8mb4;
@@ -71,7 +71,7 @@ CREATE TABLE `book`  (
 -- ----------------------------
 -- Records of book
 -- ----------------------------
-INSERT INTO `book` VALUES (1, '1231', '12314', '123', '002', '2025-05-11', '123123', NULL, NULL, '/bapi/upload/fbbfd769-680f-4e38-bebe-b7fdebcbd858.png', 344, 0, 0.0, 0, '2025-05-13 19:28:00', '2025-05-13 19:33:03', 0);
+INSERT INTO `book` VALUES (1, '1231', '12314', '123', '002', '2025-05-11', '123123', NULL, NULL, '/bapi/upload/fbbfd769-680f-4e38-bebe-b7fdebcbd858.png', 342, 3, 0.0, 0, '2025-05-13 19:28:00', '2025-05-14 11:35:26', 0);
 INSERT INTO `book` VALUES (2, '1231', '12314', '123', '002', '2025-05-11', '123123', NULL, 'hahsahdwbidbnaiwndfsivifdndfueifnesiufnseiufhsiufnsidhfnseuifheisufhsienwkjnriwneiufnisdufhis777', '/bapi/upload/d7321cca-48e9-455d-ac55-9bb4cf4d1968.png', 344, 0, 0.0, 0, '2025-05-13 19:30:55', '2025-05-13 19:36:10', 0);
 
 -- ----------------------------
@@ -119,6 +119,7 @@ CREATE TABLE `book_collection`  (
 -- ----------------------------
 -- Records of book_collection
 -- ----------------------------
+INSERT INTO `book_collection` VALUES (4, 6, 1, '2025-05-14 19:51:44');
 
 -- ----------------------------
 -- Table structure for book_comment
@@ -134,7 +135,8 @@ CREATE TABLE `book_comment`  (
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   `deleted` tinyint NOT NULL DEFAULT 0 COMMENT '是否删除：0-未删除，1-已删除',
-  `status` tinyint NOT NULL COMMENT '状态：0-待审核, 1-审核通过，2-审核拒绝',
+  `status` tinyint NOT NULL DEFAULT 0 COMMENT '状态：0-待审核, 1-审核通过，2-审核拒绝',
+  `star` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `idx_user_id`(`user_id` ASC) USING BTREE,
   INDEX `idx_book_id`(`book_id` ASC) USING BTREE,
@@ -144,6 +146,12 @@ CREATE TABLE `book_comment`  (
 -- ----------------------------
 -- Records of book_comment
 -- ----------------------------
+INSERT INTO `book_comment` VALUES (1, 6, 1, 'adwdawdawdadwvvv', NULL, 0, '2025-05-14 16:45:44', '2025-05-14 16:45:44', 0, 1, '4');
+INSERT INTO `book_comment` VALUES (2, 6, 1, 'njkvnsensivseiunfsiuenfis', NULL, 0, '2025-05-14 16:46:29', '2025-05-14 16:46:29', 0, 1, '4');
+INSERT INTO `book_comment` VALUES (3, 6, 1, '56756756756576757', NULL, 0, '2025-05-14 16:47:18', '2025-05-14 16:47:18', 0, 0, '4');
+INSERT INTO `book_comment` VALUES (4, 6, 1, 'c aodwdawdwdv', NULL, 0, '2025-05-14 16:49:18', '2025-05-14 16:49:18', 0, 2, '3');
+INSERT INTO `book_comment` VALUES (5, 6, 1, '你好哇是怎么样', 2, 0, '2025-05-14 17:28:47', '2025-05-14 17:28:47', 0, 1, '3');
+INSERT INTO `book_comment` VALUES (6, 6, 1, '啊啊', 5, 0, '2025-05-14 17:44:48', '2025-05-14 17:44:48', 0, 1, '3');
 
 -- ----------------------------
 -- Table structure for book_similarity
@@ -170,25 +178,30 @@ CREATE TABLE `borrow_record`  (
   `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '借阅记录ID',
   `user_id` bigint UNSIGNED NOT NULL COMMENT '用户ID',
   `book_id` bigint UNSIGNED NOT NULL COMMENT '图书ID',
-  `borrow_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '借阅时间',
-  `due_time` datetime NOT NULL COMMENT '应还时间',
-  `return_time` datetime NULL DEFAULT NULL COMMENT '实际归还时间',
+  `borrow_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '借阅时间',
+  `return_date` datetime NOT NULL COMMENT '应还时间',
+  `actual_return_date` datetime NULL DEFAULT NULL COMMENT '实际归还时间',
   `renewed` tinyint NOT NULL DEFAULT 0 COMMENT '是否已续借：0-否，1-是',
   `status` tinyint NOT NULL DEFAULT 0 COMMENT '状态：0-待审核, 1-借阅中，2-已归还，3-逾期未还',
   `rating` tinyint NULL DEFAULT NULL COMMENT '还书时评分(1-5)',
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   `deleted` tinyint NOT NULL DEFAULT 0 COMMENT '是否删除：0-未删除，1-已删除',
+  `renew_times` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `idx_user_id`(`user_id` ASC) USING BTREE,
   INDEX `idx_book_id`(`book_id` ASC) USING BTREE,
   INDEX `idx_status`(`status` ASC) USING BTREE,
-  INDEX `idx_due_time`(`due_time` ASC) USING BTREE
+  INDEX `idx_due_time`(`return_date` ASC) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '借阅记录表' ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of borrow_record
 -- ----------------------------
+INSERT INTO `borrow_record` VALUES (1, 6, 1, '2025-05-14 18:25:47', '2025-07-14 18:25:47', NULL, 0, 0, NULL, '2025-05-14 18:25:47', '2025-05-14 11:39:10', 0, '0');
+INSERT INTO `borrow_record` VALUES (2, 6, 1, '2025-05-14 18:26:51', '2025-07-14 18:26:51', NULL, 0, 1, NULL, '2025-05-14 18:26:51', '2025-05-14 11:39:08', 0, '0');
+INSERT INTO `borrow_record` VALUES (3, 6, 1, '2025-05-14 18:32:16', '2025-08-13 18:32:16', NULL, 0, 1, NULL, '2025-05-14 18:32:16', '2025-05-14 11:39:08', 0, '1');
+INSERT INTO `borrow_record` VALUES (4, 6, 1, '2025-05-14 18:47:21', '2025-06-21 10:46:57', '2025-05-14 19:31:56', 0, 2, NULL, '2025-05-14 18:47:21', '2025-05-14 18:47:21', 0, '0');
 
 -- ----------------------------
 -- Table structure for user
@@ -218,7 +231,7 @@ CREATE TABLE `user`  (
 -- ----------------------------
 INSERT INTO `user` VALUES (1, 'admin', 'e10adc3949ba59abbe56e057f20f883e', '系统管理员', NULL, NULL, NULL, 1, 0, 0, '2025-05-12 08:51:55', '2025-05-12 10:21:58', 0);
 INSERT INTO `user` VALUES (6, '003', 'c6f057b86584942e415435ffb1fa93d4', 'aaa', NULL, NULL, '/bapi/upload/6c220cf7-587f-4771-9f98-d2a2317a23d2.png', 0, 0, 0, '2025-05-13 15:27:15', '2025-05-13 15:27:15', 0);
-INSERT INTO `user` VALUES (7, '001', 'c6f057b86584942e415435ffb1fa93d4', 'aaw', NULL, NULL, NULL, 0, 0, 0, '2025-05-13 15:28:28', '2025-05-13 17:11:57', 1);
+INSERT INTO `user` VALUES (7, '001', 'c6f057b86584942e415435ffb1fa93d4', 'aaw', NULL, NULL, NULL, 0, 0, 0, '2025-05-13 15:28:28', '2025-05-14 11:15:35', 0);
 INSERT INTO `user` VALUES (8, '002', 'c6f057b86584942e415435ffb1fa93d4', 'aa', NULL, NULL, NULL, 0, 0, 0, '2025-05-13 15:29:54', '2025-05-13 17:59:58', 1);
 INSERT INTO `user` VALUES (9, 'r001', 'c6f057b86584942e415435ffb1fa93d4', 'aaa', NULL, NULL, '/bapi/upload/2b244163-e7bb-411d-bf21-1160b1439b5c.png', 1, 0, 0, '2025-05-13 17:51:45', '2025-05-13 17:51:45', 0);
 
